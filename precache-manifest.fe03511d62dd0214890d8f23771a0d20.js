a14dc54f981aa9cdd01fc376f204742b6e9d472c
WuWooLay <mamamglay.lp@gmail.com>
2019-05-26 08:57:44 +0630

    Add Build Data

---
+++ precache-manifest.fe03511d62dd0214890d8f23771a0d20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e6966b04388febc73aaaab0147d0c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80e3eedb17f19fa8ed57",
+    "url": "/static/css/main.5dc7dc1d.chunk.css"
+  },
+  {
+    "revision": "8aff313b8458d896961d",
+    "url": "/static/js/2.f9249cdd.chunk.js"
+  },
+  {
+    "revision": "80e3eedb17f19fa8ed57",
+    "url": "/static/js/main.8435a972.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.210d061f360ef8127ccdbed49a68511f.js"
+  "/precache-manifest.fe03511d62dd0214890d8f23771a0d20.js"
 );
 
 self.addEventListener('message', (event) => {
